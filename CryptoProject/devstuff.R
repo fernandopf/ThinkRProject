666e4e893ae209f66c3bfaa1eb70dbdba51aec86
fernandopf <fernandoperez94@gmail.com>
2018-11-25 20:33:02 +0100

    Added function averages to the package

--- CryptoProject/devstuff.R
+++ CryptoProject/devstuff.R
@@ -4,12 +4,12 @@ usethis::use_mit_license()
 
 #vignette
 usethis::use_vignette("userguide")
-
+utils::globalVariables(c("."), add = FALSE)
 # Dependencies
 usethis::use_package("dplyr")
 usethis::use_package("jsonlite")
 usethis::use_package("lubridate")
-#usethis::use_package("pracma")
+usethis::use_package("pracma")
 usethis::use_pipe()
 usethis::use_package("curl")
 usethis::use_package("utils")
