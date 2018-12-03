4a008c973321b2d81d4ee163a94008752aa6aacd
fernandopf <fernandoperez94@gmail.com>
2018-12-03 19:07:16 +0100

    Updated functions day_hour and lastweek_minute to include when happen when cryptocurrency and compare are the same

--- CryptoProject/R/day_hour.R
+++ CryptoProject/R/day_hour.R
@@ -3,11 +3,11 @@
 #'
 #' Function to get the information from the cryptocompare API
 #'
-#' This function has been designed to get a dataframe from the crypocurrency API with the highest, lowest, open and close price from the crypocurrency chosen in hour or day timeframe.
+#' This function has been designed to get a dataframe from the cryptocurrency API with the highest, lowest, open and close price from the cryptocurrency chosen in hour or day timeframe.
 #' @param timeframe timeframe in hour or day
 #' @param firstDay first day to analyse in dd/mm/yyyy format
 #' @param lastDay last day to analyse in dd/mm/yyyy format
-#' @param crypocurrency cryptocurrency to analyse
+#' @param cryptocurrency cryptocurrency to analyse
 #' @param comparison currency to be compared
 #'
 #' @return dataframe with the time, highest price, lowest price, open price, close price of the chosen timeframe
@@ -18,7 +18,7 @@
 
 #' @return dataframe with all the information required
 #' @examples
-day_hour <- function(timeframe, firstDay, lastDay, crypocurrency = "BTC", comparison = "USD") {
+day_hour <- function(timeframe, firstDay, lastDay, cryptocurrency = "BTC", comparison = "USD") {
 
   # Date
   firstDay <- as.Date(firstDay,format="%d/%m/%Y")
@@ -42,37 +42,28 @@ day_hour <- function(timeframe, firstDay, lastDay, crypocurrency = "BTC", compar
     print('No valid timeframe')
     return();
   }
+  # If the cryptocurrency and the comparsion are the same, we return one dataset with all the prices equal to 1 and the volume equal to 0.
 
-  # Maximum number of points is 2000
-  if (n <= 2000) {
-    link <- glue("https://min-api.cryptocompare.com/data/{a}?fsym={crypocurrency}&tsym={comparison}&limit={n}&aggregate=1&toTs={time}&extraParams=ThinkR")
-    linkVolume <- glue("https://min-api.cryptocompare.com/data/exchange/{a}?tsym={crypocurrency}&limit={n}&toTs={time}&extraParams=ThinkR")
-    dataPrice <- fromJSON(link)
-    dataVolume <- fromJSON(linkVolume)
+
+  if (cryptocurrency == comparison){
+    dates <- sapply(0:n, function(i){
+      as.numeric(as.POSIXct(firstDay, format="%m/%d/%Y")) +(i)*incr
+    })
     df <- data.frame(
-      date= as.POSIXct(dataPrice$Data$time,origin = "1970-01-01",tz = "GMT"),
-      high=dataPrice$Data$high,
-      low = dataPrice$Data$low,
-      open = dataPrice$Data$open,
-      close = dataPrice$Data$close,
-      volume = dataVolume$Data$volume
-    )
-  }
-  # If the number of points is higher than the maximum we need to do a for loop
-  else {
-    # Round to the highest Integuer
-    iterations <- ceiling(n/2000)
-    n1 <- 2000
-    # For to get data from the API several times
-    for (i in 1:iterations){
-      if (i ==iterations){
-        n1 =n-2000*(iterations-1)
-      }
-      linkPrice <- glue("https://min-api.cryptocompare.com/data/{a}?fsym={crypocurrency}&tsym={comparison}&limit={n1}&aggregate=1&toTs={time}&extraParams=ThinkR")
-      linkVolume <- glue("https://min-api.cryptocompare.com/data/exchange/{a}?tsym={crypocurrency}&limit={n1}&toTs={time}&extraParams=ThinkR")
-      dataPrice <- fromJSON(linkPrice)
+      date= as.POSIXct(dates,origin = "1970-01-01",tz = "GMT"),
+      high=1,
+      low = 1,
+      open = 1,
+      close = 1,
+      volume = 0)
+  } else {
+    # Maximum number of points is 2000
+    if (n <= 2000) {
+      link <- glue("https://min-api.cryptocompare.com/data/{a}?fsym={cryptocurrency}&tsym={comparison}&limit={n}&aggregate=1&toTs={time}&extraParams=ThinkR")
+      linkVolume <- glue("https://min-api.cryptocompare.com/data/exchange/{a}?tsym={cryptocurrency}&limit={n}&toTs={time}&extraParams=ThinkR")
+      dataPrice <- fromJSON(link)
       dataVolume <- fromJSON(linkVolume)
-      df1 <- data.frame(
+      df <- data.frame(
         date= as.POSIXct(dataPrice$Data$time,origin = "1970-01-01",tz = "GMT"),
         high=dataPrice$Data$high,
         low = dataPrice$Data$low,
@@ -80,12 +71,36 @@ day_hour <- function(timeframe, firstDay, lastDay, crypocurrency = "BTC", compar
         close = dataPrice$Data$close,
         volume = dataVolume$Data$volume
       )
-      if (i ==1){
-        df <- df1
-      } else {
-        df <- rbind(df, df1)
+    }
+    # If the number of points is higher than the maximum we need to do a for loop
+    else {
+      # Round to the highest Integuer
+      iterations <- ceiling(n/2000)
+      n1 <- 2000-1
+      # For to get data from the API several times
+      for (i in 1:iterations){
+        if (i ==iterations){
+          n1 =n-2000*(iterations-1)-1
+        }
+        linkPrice <- glue("https://min-api.cryptocompare.com/data/{a}?fsym={cryptocurrency}&tsym={comparison}&limit={n1}&aggregate=1&toTs={time}&extraParams=ThinkR")
+        linkVolume <- glue("https://min-api.cryptocompare.com/data/exchange/{a}?tsym={cryptocurrency}&limit={n1}&toTs={time}&extraParams=ThinkR")
+        dataPrice <- fromJSON(linkPrice)
+        dataVolume <- fromJSON(linkVolume)
+        df1 <- data.frame(
+          date= as.POSIXct(dataPrice$Data$time,origin = "1970-01-01",tz = "GMT"),
+          high=dataPrice$Data$high,
+          low = dataPrice$Data$low,
+          open = dataPrice$Data$open,
+          close = dataPrice$Data$close,
+          volume = dataVolume$Data$volume
+        )
+        if (i ==1){
+          df <- df1
+        } else {
+          df <- rbind(df, df1)
+        }
+        time <- time - 2000*incr
       }
-      time <- time - 2000*incr
     }
   }
   df <-df %>%
