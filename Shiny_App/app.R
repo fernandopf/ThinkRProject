84e8b866a9931f3752be73378e0a3818c13d7b1c
Jiahao Wang <dlxmigue@gmail.com>
2018-12-05 20:58:44 +0100

    Shiny 1.5 Main tab totally functional

--- Shiny_App/app.R
+++ Shiny_App/app.R
@@ -56,9 +56,9 @@ ui <- fluidPage(
          fluidRow(
            #Moving average
            column(3, wellPanel(
-             numericInput( "MA",
+             numericInput( inputId= "MA",
                            label = "Moving Average (MA)",
-                           value = 5, min = 1, max = 85, step = 1
+                           value = 5, min = 2, max = 85, step = 1)
              )
            )
            ),
@@ -66,8 +66,25 @@ ui <- fluidPage(
            column(3, wellPanel(
              actionButton(inputId = "MACD", label = "MACD"),
              actionButton(inputId = "VOLUME", "volume")
-           )
-           )
+                  )
+           ),
+           column(3, wellPanel(
+             numericInput( inputId= "slow_MA",
+                           label = "Slow Moving Average",
+                           value = 26, min = 2, max = 85, step = 1)
+                )
+            ),
+         column(3, wellPanel(
+           numericInput( inputId= "quick_MA",
+                         label = "Quick Moving Average",
+                         value = 12, min = 2, max = 85, step = 1)
+         )
+         ),
+         column(3, wellPanel(
+           numericInput( inputId= "signal_MA",
+                         label = "Signal Moving Average",
+                         value = 9, min = 2, max = 85, step = 1)
+         )
          ),
          mainPanel(
            plotlyOutput("mainPlot")
@@ -93,25 +110,74 @@ server <- function(input, output, session){
     coin = "BTC",
     compare = "USD",
     ma = 5,
-    quick_MACD = 12,
-    slow_MACD = 26,
-    signal_MACD = 9
+    quick_MA = 12,
+    slow_MA = 26,
+    signal_MA = 9
   )
   
   observe({
-    param$frame <- input$timeframe
-    param$timerange <- input$timerange
     param$coin <- input$currency
     param$compare <- input$comparison
     param$ma <- input$MA
+    param$slow_MA <- input$slow_MA
+    param$quick_MA <- input$quick_MA
+    param$signal_MA <- input$signal_MA
   })
   
+  #Boundary for maximum number of points to compute the moving average
+  observe({
+    param$timerange <- input$timerange
+    param$frame <- input$timeframe
+    
+    if (input$timeframe == "day"){
+      updateNumericInput(session = session, inputId = "MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
+      updateNumericInput(session = session, inputId = "slow_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
+      updateNumericInput(session = session, inputId = "quick_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
+      updateNumericInput(session = session, inputId = "signal_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
+    }
+    else if (input$timeframe == "month"){
+      updateNumericInput(session = session, inputId = "MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
+      updateNumericInput(session = session, inputId = "slow_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
+      updateNumericInput(session = session, inputId = "quick_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
+      updateNumericInput(session = session, inputId = "signal_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
+    }
+    else if (input$timeframe == "week"){
+      updateNumericInput(session = session, inputId = "MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
+      updateNumericInput(session = session, inputId = "slow_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
+      updateNumericInput(session = session, inputId = "quick_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
+      updateNumericInput(session = session, inputId = "signal_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
+    }
+    else if (input$timeframe == "hour"){
+      updateNumericInput(session = session, inputId = "MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
+      updateNumericInput(session = session, inputId = "slow_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
+      updateNumericInput(session = session, inputId = "quick_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
+      updateNumericInput(session = session, inputId = "signal_MA", 
+                         max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
+    }
+  })
+  
+  #Options of timeframe when the time range is changed
   observe({
     if( as.numeric(input$timerange[2] - input$timerange[1]) > 24 * 30 ){
             updateSelectInput(session, inputId = "timeframe", label = "Choose first and last day: ",
                         selected = "month", choices = c("Month" = "month", "Week" = "week", "Day" = "day") )
         }
-        #If the timerange is bigger than 3 months but smaller than a year, timeframe can be month, week or day
+        #If the timerange is bigger than 3 months but smaller than a year, timeframe can be week or day
         else if( as.numeric(input$timerange[2] - input$timerange[1]) > 24 * 7 ){
           updateSelectInput(session, inputId = "timeframe", label = "Choose first and last day: ",
                         selected = "week", choices = c( "Week" = "week", "Day" ="day") )
@@ -139,11 +205,14 @@ server <- function(input, output, session){
     coin <- param$coin
     compare <- param$compare
     ma <- param$ma
+    slow_ma <- param$slow_MA 
+    quick_ma <- param$quick_MA 
+    signal_ma <- param$signal_MA
     
     df <- crypto(timeframe = frame, firstDay = format(start, "%d/%m/%Y"), 
                  lastDay = format(end, "%d/%m/%Y"), 
                  cryptocurrency = coin, comparison = compare, 
-                 n_MA = ma, n_quick_MACD = 12, n_slow_MACD = 26, n_signal_MACD = 9)
+                 n_MA = ma, n_quick_MACD = quick_ma, n_slow_MACD = slow_ma, n_signal_MACD = signal_ma)
     
     candle_plot(df, MACD)
   })
@@ -158,11 +227,14 @@ server <- function(input, output, session){
       coin <- param$coin
       compare <- param$compare
       ma <- param$ma
+      slow_ma <- param$slow_MA 
+      quick_ma <- param$quick_MA 
+      signal_ma <- param$signal_MA
 
       df <- crypto(timeframe = frame, firstDay = format(start, "%d/%m/%Y"),
                   lastDay = format(end, "%d/%m/%Y"),
                   cryptocurrency = coin, comparison = compare,
-                  n_MA = ma, n_quick_MACD = 12, n_slow_MACD = 26, n_signal_MACD = 9)
+                  n_MA = ma, n_quick_MACD = quick_ma, n_slow_MACD = slow_ma, n_signal_MACD = signal_ma)
       candle_plot(df, MACD)
     })
   })
@@ -177,11 +249,14 @@ server <- function(input, output, session){
       coin <- param$coin
       compare <- param$compare
       ma <- param$ma
+      slow_ma <- param$slow_MA 
+      quick_ma <- param$quick_MA 
+      signal_ma <- param$signal_MA
 
       df <- crypto(timeframe = frame, firstDay = format(start, "%d/%m/%Y"),
                    lastDay = format(end, "%d/%m/%Y"),
                    cryptocurrency = coin, comparison = compare,
-                   n_MA = ma, n_quick_MACD = 12, n_slow_MACD = 26, n_signal_MACD = 9)
+                   n_MA = ma, n_quick_MACD = quick_ma, n_slow_MACD = slow_ma, n_signal_MACD = signal_ma)
       candle_plot(df, volume)
     })
   })
