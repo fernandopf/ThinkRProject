86c8a9ae894e23e9c7f46cf72be1ae9f9d0b37b5
Jiahao Wang <dlxmigue@gmail.com>
2018-12-06 00:50:15 +0100

    Shiny 1.6 Final layout of first tab and file with reauired packages for Shiny

--- Shiny_App/app.R
+++ Shiny_App/app.R
@@ -4,7 +4,7 @@ library(shinythemes)
 library(markdown)
 library(CryptoProject)
 library(plotly)
-
+library(shinydashboard) #Make tabs as buttons
 
 #Reading all possible currencies from CSV
 name_coins <- read.csv("data/coins.csv", header = FALSE)[,1]
@@ -13,6 +13,7 @@ names(all_coins) <- name_coins
 
 #User interface
 ui <- fluidPage(
+   #Theme
    theme = shinytheme("sandstone"),
    
    # Application title
@@ -21,75 +22,73 @@ ui <- fluidPage(
    navbarPage("CrytoProject!",
     #-------------------------------1st tabPanel--------------------------------------
       tabPanel("Plot",
-         
-         fluidRow(
-           
-           #Currency selector
-           column(3, wellPanel(
-             selectInput(inputId = "currency", label = "Currency you are interested? ",
-                         selected = "BTC", choices = all_coins )
+               
+        sidebarLayout(
+          #Sidebar Layout-----------------
+          sidebarPanel(
+            
+            #Currency we are interested in
+            selectInput(inputId = "currency", label = "Currency you are interested? ",
+                        selected = "BTC", choices = all_coins ),
+            
+            #Currency we want to compare with
+            selectInput(inputId = "comparison", label = "Currency you want to compare? ",
+                        selected = "USD", choices = all_coins ),
+            
+            #Time range we we are interested in
+            dateRangeInput(inputId = "timerange", 
+                           label = "Choose first and last day: ",
+                           start = Sys.Date() - 90, end = Sys.Date(),
+                           separator = " - ", format = "dd/mm/yy",
+                           startview = 'year', language = 'fr', weekstart = 1),
+            
+            #Size of a point: hour, days, weeks or month
+            selectInput(inputId = "timeframe", label = "Choose first and last day: ",
+                        selected = "day", choices = c("Day" = "day") ) ,
+            
+            #Number of points used to compute moving average
+            numericInput( inputId= "MA",
+                          label = "Moving Average (MA)",
+                          value = 5, min = 2, max = 85, step = 1),
+          
+            #Type of supplementary plot: MACD or volume
+            tags$b("Choose type supplementary plot:" ),
+            
+            fluidRow(
+                tabBox( id = "tabset", selected = "MACD", side = "left",
+                        tabPanel(title = "MACD", value = "MACD",
+                                 
+                                 #Number of points used to compute the slow moving average
+                                 numericInput( inputId= "slow_MA",
+                                               label = "Slow Moving Average",
+                                               value = 26, min = 2, max = 85, step = 1),
+                                 
+                                 #Number of points used to compute quick moving average
+                                 numericInput( inputId= "quick_MA",
+                                               label = "Quick Moving Average",
+                                               value = 12, min = 2, max = 85, step = 1),
+                                 
+                                 #Number of points used to compute signal moving average
+                                 numericInput( inputId= "signal_MA",
+                                               label = "Signal Moving Average",
+                                               value = 9, min = 2, max = 85, step = 1)
+                                 ),
+                        
+                          tabPanel(title = "VOLUME", value = "VOLUME")
+                        
+                      ) #End of tabBox
                     )
-           ),
-           #Currency to compare with
-           column(3, wellPanel(
-             selectInput(inputId = "comparison", label = "Currency you want to compare? ",
-                         selected = "USD", choices = all_coins )
-                    )
-           ),
-           # Timerange selector
-           column(3, wellPanel(
-             dateRangeInput(inputId = "timerange", 
-                            label = "Choose first and last day: ",
-                            start = Sys.Date() - 90, end = Sys.Date(),
-                            separator = " - ", format = "dd/mm/yy",
-                            startview = 'year', language = 'fr', weekstart = 1)
-                    )
-          ),
-          #Timeframe selector
-           column(3, wellPanel(
-             selectInput(inputId = "timeframe", label = "Choose first and last day: ",
-                         selected = "day", choices = c("Day" = "day") )
-                    )
-           )
-          ), #End of fluidRow
-         
-         fluidRow(
-           #Moving average
-           column(3, wellPanel(
-             numericInput( inputId= "MA",
-                           label = "Moving Average (MA)",
-                           value = 5, min = 2, max = 85, step = 1)
-             )
-           )
-           ),
-           #Supplementary plot choice
-           column(3, wellPanel(
-             actionButton(inputId = "MACD", label = "MACD"),
-             actionButton(inputId = "VOLUME", "volume")
-                  )
-           ),
-           column(3, wellPanel(
-             numericInput( inputId= "slow_MA",
-                           label = "Slow Moving Average",
-                           value = 26, min = 2, max = 85, step = 1)
-                )
-            ),
-         column(3, wellPanel(
-           numericInput( inputId= "quick_MA",
-                         label = "Quick Moving Average",
-                         value = 12, min = 2, max = 85, step = 1)
-         )
-         ),
-         column(3, wellPanel(
-           numericInput( inputId= "signal_MA",
-                         label = "Signal Moving Average",
-                         value = 9, min = 2, max = 85, step = 1)
-         )
-         ),
-         mainPanel(
+
+          ), #End of Sidebar Panel-----------------
+          
+          #Main Panel: Plot
+          mainPanel(
            plotlyOutput("mainPlot")
          )
-      ),
+      ) #End of Sidebar Layout
+      
+    ), #End of first tab
+    
     #------------------------------------2nd tabPanel-------------------------------------
     tabPanel("Other things"),
     navbarMenu("More", 
@@ -97,12 +96,17 @@ ui <- fluidPage(
       tabPanel("About")
     )
    ) #End NavBar
-)
+   
+)#End of UI-------------------------
+
+
+
 
 #----------------------------------------------------------------------------------
 # Server of Shiny App
 server <- function(input, output, session){
   
+  #All parameters for 1st tab
   param <- reactiveValues(
     frame = "day",
     start = Sys.Date() - 90,
@@ -130,60 +134,65 @@ server <- function(input, output, session){
     param$frame <- input$timeframe
     
     if (input$timeframe == "day"){
-      updateNumericInput(session = session, inputId = "MA", 
+
+
+      updateNumericInput(session = session, inputId = "MA",
+                         max =  as.numeric(input$timerange[2]-input$timerange[1]) -5)
+      updateNumericInput(session = session, inputId = "slow_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
-      updateNumericInput(session = session, inputId = "slow_MA", 
+      updateNumericInput(session = session, inputId = "quick_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
-      updateNumericInput(session = session, inputId = "quick_MA", 
-                         max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
-      updateNumericInput(session = session, inputId = "signal_MA", 
+      updateNumericInput(session = session, inputId = "signal_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1]) -5 )
     }
     else if (input$timeframe == "month"){
-      updateNumericInput(session = session, inputId = "MA", 
+
+      updateNumericInput(session = session, inputId = "MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
-      updateNumericInput(session = session, inputId = "slow_MA", 
+      updateNumericInput(session = session, inputId = "slow_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
-      updateNumericInput(session = session, inputId = "quick_MA", 
+      updateNumericInput(session = session, inputId = "quick_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
-      updateNumericInput(session = session, inputId = "signal_MA", 
+      updateNumericInput(session = session, inputId = "signal_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/31 -5 )
     }
     else if (input$timeframe == "week"){
-      updateNumericInput(session = session, inputId = "MA", 
+
+      updateNumericInput(session = session, inputId = "MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
-      updateNumericInput(session = session, inputId = "slow_MA", 
+      updateNumericInput(session = session, inputId = "slow_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
-      updateNumericInput(session = session, inputId = "quick_MA", 
+      updateNumericInput(session = session, inputId = "quick_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
-      updateNumericInput(session = session, inputId = "signal_MA", 
+      updateNumericInput(session = session, inputId = "signal_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])/7 -5 )
     }
     else if (input$timeframe == "hour"){
-      updateNumericInput(session = session, inputId = "MA", 
+
+      updateNumericInput(session = session, inputId = "MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
-      updateNumericInput(session = session, inputId = "slow_MA", 
+      updateNumericInput(session = session, inputId = "slow_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
-      updateNumericInput(session = session, inputId = "quick_MA", 
+      updateNumericInput(session = session, inputId = "quick_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
-      updateNumericInput(session = session, inputId = "signal_MA", 
+      updateNumericInput(session = session, inputId = "signal_MA",
                          max = as.numeric(input$timerange[2]-input$timerange[1])*24 -5 )
     }
   })
   
   #Options of timeframe when the time range is changed
   observe({
-    if( as.numeric(input$timerange[2] - input$timerange[1]) > 24 * 30 ){
+    if( as.numeric(input$timerange[2] - input$timerange[1]) > 26 * 30 ){
             updateSelectInput(session, inputId = "timeframe", label = "Choose first and last day: ",
                         selected = "month", choices = c("Month" = "month", "Week" = "week", "Day" = "day") )
         }
         #If the timerange is bigger than 3 months but smaller than a year, timeframe can be week or day
-        else if( as.numeric(input$timerange[2] - input$timerange[1]) > 24 * 7 ){
+        else if( as.numeric(input$timerange[2] - input$timerange[1]) > 26 * 7 ){
           updateSelectInput(session, inputId = "timeframe", label = "Choose first and last day: ",
                         selected = "week", choices = c( "Week" = "week", "Day" ="day") )
         }
         #If the timerange is bigger than 1 month but smaller than 3, timeframe can only be week or day
-        else if( as.numeric(input$timerange[2] - input$timerange[1]) > 24 ){
+        else if( as.numeric(input$timerange[2] - input$timerange[1]) > 26 ){
           updateSelectInput(session, inputId = "timeframe", label = "Choose first and last day: ",
                         selected = "day", choices = c( "Day" = "day") )
         }
@@ -195,7 +204,7 @@ server <- function(input, output, session){
   })
 
   #------------------------------------------------------------------------------------
-
+  
   #First tab plot: Currency prices, moving averages,...; default MACD
   output$mainPlot <- renderPlotly({
     
@@ -217,54 +226,52 @@ server <- function(input, output, session){
     candle_plot(df, MACD)
   })
   
-  #Change main plot if user choose MACD
-  observeEvent(input$MACD, {
-    output$mainPlot <- renderPlotly({
-      
-      frame <- param$frame
-      start <- param$timerange[1]
-      end <- param$timerange[2]
-      coin <- param$coin
-      compare <- param$compare
-      ma <- param$ma
-      slow_ma <- param$slow_MA 
-      quick_ma <- param$quick_MA 
-      signal_ma <- param$signal_MA
-
-      df <- crypto(timeframe = frame, firstDay = format(start, "%d/%m/%Y"),
-                  lastDay = format(end, "%d/%m/%Y"),
-                  cryptocurrency = coin, comparison = compare,
-                  n_MA = ma, n_quick_MACD = quick_ma, n_slow_MACD = slow_ma, n_signal_MACD = signal_ma)
-      candle_plot(df, MACD)
-    })
-  })
+#Transforming plot when MACD or VOLUME tab selected
+  observe({
+    frame <- param$frame
+    start <- param$timerange[1]
+    end <- param$timerange[2]
+    coin <- param$coin
+    compare <- param$compare
+    ma <- param$ma
+    slow_ma <- param$slow_MA 
+    quick_ma <- param$quick_MA 
+    signal_ma <- param$signal_MA
+    
+    #Change main plot if user choose MACD
+    if (input$tabset == "MACD") {
   
-  #Change main plot if user choose VOLUME
-  observeEvent(input$VOLUME, {
     output$mainPlot <- renderPlotly({
       
-      frame <- param$frame
-      start <- param$timerange[1]
-      end <- param$timerange[2]
-      coin <- param$coin
-      compare <- param$compare
-      ma <- param$ma
-      slow_ma <- param$slow_MA 
-      quick_ma <- param$quick_MA 
-      signal_ma <- param$signal_MA
+      df <- crypto(timeframe = frame, firstDay = format(start, "%d/%m/%Y"),
+                   lastDay = format(end, "%d/%m/%Y"),
+                   cryptocurrency = coin, comparison = compare,
+                   n_MA = ma, n_quick_MACD = quick_ma, n_slow_MACD = slow_ma, n_signal_MACD = signal_ma)
+    
+      candle_plot(df, MACD)
+      }) #End of render
+    } #End of if
+    
+    #Change main plot if user choose VOLUME
+    else if (input$tabset == "VOLUME") {
+    
+    output$mainPlot <- renderPlotly({
 
       df <- crypto(timeframe = frame, firstDay = format(start, "%d/%m/%Y"),
                    lastDay = format(end, "%d/%m/%Y"),
                    cryptocurrency = coin, comparison = compare,
                    n_MA = ma, n_quick_MACD = quick_ma, n_slow_MACD = slow_ma, n_signal_MACD = signal_ma)
+      
       candle_plot(df, volume)
-    })
-  })
+    }) #End of render
+    }#End of else if
+  }) #End of observer for plot transformation due to tab selection
+
   
-
-
-
-}
+  
+  
+  
+} #End of server---------
 
 # Run the application 
 shinyApp(ui = ui, server = server)
